8a5375a050a80fc6bd39d3b350be5e60b5836444
orilevy650 <orishalom.levi@gmail.com>
2025-10-21 19:50:14 +0300

    Completed Caesar cipher encryption and decryption

--- src/caesar_encrypt.py
+++ src/caesar_encrypt.py
@@ -1,7 +1,25 @@
 # main.py
 def caesar_encrypt(text, shift):
     result = ""
+    for char in text:
+        if char.islower():
+            result += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+        elif char.isupper():
+            result += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+        else:
+            result += char
     return result
+def crack_caesar(file_in, file_out):
+    with open(file_in, "r") as f:
+        secret_text = f.read()
+    for shift in range(26):
+        decrypted = caesar_encrypt(secret_text, -shift)
+        if " " in decrypted:
+            with open(file_out, "w") as f_out:
+                f_out.write(decrypted)
+            print(f"Decrypted with shift={shift}:")
+            print(decrypted)
+            break
 
 if __name__ == "__main__":
     original_text = "hello world"
